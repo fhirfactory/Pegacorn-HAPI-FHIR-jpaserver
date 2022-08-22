736e2cd557c1f7f0762bcdd5b19eff20f80b4cfd
akinboj <akinboj@hotmail.com>
2022-08-22 16:47:51 +1000

    minor updates

--- src/main/java/ca/uhn/fhir/jpa/starter/Application.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/Application.java
@@ -1,45 +1,78 @@
 package ca.uhn.fhir.jpa.starter;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-
-import ca.uhn.fhir.context.FhirVersionEnum;
+import ca.uhn.fhir.jpa.starter.mdm.MdmConfig;
+import ca.uhn.fhir.jpa.starter.annotations.OnEitherVersion;
 import ca.uhn.fhir.jpa.subscription.channel.config.SubscriptionChannelConfig;
 import ca.uhn.fhir.jpa.subscription.match.config.SubscriptionProcessorConfig;
 import ca.uhn.fhir.jpa.subscription.match.config.WebsocketDispatcherConfig;
 import ca.uhn.fhir.jpa.subscription.submit.config.SubscriptionSubmitterConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchRestClientAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
-public class Application extends AnnotationConfigWebApplicationContext {
+@ServletComponentScan(basePackageClasses = {
+  JpaRestfulServer.class})
+@SpringBootApplication(exclude = {ElasticsearchRestClientAutoConfiguration.class})
+@Import({SubscriptionSubmitterConfig.class, SubscriptionProcessorConfig.class, SubscriptionChannelConfig.class, WebsocketDispatcherConfig.class, MdmConfig.class})
+public class Application extends SpringBootServletInitializer {
 
-@Autowired
-AppProperties appProperties;
+  public static void main(String[] args) {
 
-  public Application() {
-    FhirVersionEnum fhirVersion = appProperties.getFhir_version();
-    if (fhirVersion == FhirVersionEnum.DSTU2) {
-      register(FhirServerConfigDstu2.class, FhirServerConfigCommon.class);
-    } else if (fhirVersion == FhirVersionEnum.DSTU3) {
-      register(FhirServerConfigDstu3.class, FhirServerConfigCommon.class);
-    } else if (fhirVersion == FhirVersionEnum.R4) {
-      register(FhirServerConfigR4.class, FhirServerConfigCommon.class);
-    } else if (fhirVersion == FhirVersionEnum.R5) {
-      register(FhirServerConfigR5.class, FhirServerConfigCommon.class);
-    } else {
-      throw new IllegalStateException();
-    }
-    
-    AppProperties.Subscription nested = new AppProperties.Subscription();
-    if (nested.getWebsocket_enabled()) {
-        register(WebsocketDispatcherConfig.class);
-      }
-    
-    if (nested.getResthook_enabled()
-    	|| nested.getWebsocket_enabled()) {
-    	register(SubscriptionSubmitterConfig.class);
-    	register(SubscriptionProcessorConfig.class);
-    	register(SubscriptionChannelConfig.class);
-    	    }
+    SpringApplication.run(Application.class, args);
 
+    //Server is now accessible at eg. http://localhost:8080/fhir/metadata
+    //UI is now accessible at http://localhost:8080/
   }
 
+  @Override
+  protected SpringApplicationBuilder configure(
+    SpringApplicationBuilder builder) {
+    return builder.sources(Application.class);
+  }
+
+  @Autowired
+  AutowireCapableBeanFactory beanFactory;
+
+  @Bean
+  @Conditional(OnEitherVersion.class)
+  public ServletRegistrationBean hapiServletRegistration() {
+    ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean();
+    JpaRestfulServer jpaRestfulServer = new JpaRestfulServer();
+    beanFactory.autowireBean(jpaRestfulServer);
+    servletRegistrationBean.setServlet(jpaRestfulServer);
+    servletRegistrationBean.addUrlMappings("/fhir/*");
+    servletRegistrationBean.setLoadOnStartup(1);
+
+    return servletRegistrationBean;
+  }
+
+  @Bean
+  public ServletRegistrationBean overlayRegistrationBean() {
+
+    AnnotationConfigWebApplicationContext annotationConfigWebApplicationContext = new AnnotationConfigWebApplicationContext();
+    annotationConfigWebApplicationContext.register(FhirTesterConfig.class);
+
+    DispatcherServlet dispatcherServlet = new DispatcherServlet(
+      annotationConfigWebApplicationContext);
+    dispatcherServlet.setContextClass(AnnotationConfigWebApplicationContext.class);
+    dispatcherServlet.setContextConfigLocation(FhirTesterConfig.class.getName());
+
+    ServletRegistrationBean registrationBean = new ServletRegistrationBean();
+    registrationBean.setServlet(dispatcherServlet);
+    registrationBean.addUrlMappings("/*");
+    registrationBean.setLoadOnStartup(1);
+    return registrationBean;
+
+  }
 }
--- src/main/java/ca/uhn/fhir/jpa/starter/JpaRestfulServer.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/JpaRestfulServer.java
@@ -1,24 +1,34 @@
 package ca.uhn.fhir.jpa.starter;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+
 import javax.servlet.ServletException;
 
+@Import(AppProperties.class)
 public class JpaRestfulServer extends BaseJpaRestfulServer {
 
-  private static final long serialVersionUID = 1L;
+  @Autowired
+  AppProperties appProperties;
 
+  private static final long serialVersionUID = 1L;
   public static JpaRestfulServer INSTANCE = null;
   public static final String API_KEY_HEADER_NAME = "x-api-key";
-  
+
+  public JpaRestfulServer() {
+    super();
+  }
+
   @Override
   protected void initialize() throws ServletException {
     super.initialize();
-
+    
+ // Add your own customization here
+    
     INSTANCE = this;
-
-
     ApiKeyValidatorInterceptor apiKeyValidatorInterceptor = new ApiKeyValidatorInterceptor(API_KEY_HEADER_NAME, "HAPI_API_KEY");
     registerInterceptor(apiKeyValidatorInterceptor);
-    
+
   }
 
 }
